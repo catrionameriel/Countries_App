a6d1a3921c21d99e6ab5b10c0b619625e5bccf13
eliotrhys <eliot@eliotshort.co.uk>
2018-01-31 14:22:40 +0000

    noice

--- public/index.js
+++ public/index.js
@@ -5,25 +5,33 @@ var makeRequest = function(url, callback){
   request.send();
 }
 
-var clickToReveal = function(){
-  var url = 'https://restcountries.eu/rest/v2';
-  makeRequest(url, requestComplete);
-}
+var showCountryDetails = function(countries){
 
-var populateList = function(countries){
+  var selectedCountry = this;
+console.log(countries);
+  var the_country = null;
+
+  for (country of countries){
+    if (this.value === country.name){
+      return country
+    }
+    var the_country = country;
+  }
+
   var ul = document.querySelector('#country-list');
 
-  countries.forEach(function(country){
-    var li = document.createElement("li");
-    li.innerText = country.name;
-    ul.appendChild(li);
-  })
-}
+  var li = document.createElement("li");
+  li.innerText = the_country.name;
 
-var populateOptions = function(countries){
-  var select = document.querySelector("#country-dropdown");
+  var li2 = document.createElement("li");
+  li2.innerText = the_country.population;
 
+  var li3 = document.createElement("li");
+  li3.innerText = the_country.capital;
 
+  ul.appendChild(li);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
 
 }
 
@@ -31,12 +39,51 @@ var requestComplete = function(){
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
-  populateList(countries);
+  populateDropdown(countries);
+
+  var dropDown = document.querySelector('#country-dropdown');
+
+  dropDown.addEventListener('change', function() {
+    var the_country = null;
+    for (country of countries){
+      console.log(country);
+      if (this.value === country.name){
+        var the_country = country;
+      }
+    }
+
+    var ul = document.querySelector('#country-list');
+
+    var li = document.createElement("li");
+    li.innerText = the_country.name;
+
+    var li2 = document.createElement("li");
+    li2.innerText = the_country.population;
+
+    var li3 = document.createElement("li");
+    li3.innerText = the_country.capital;
+
+    ul.appendChild(li);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+  });
+}
+
+var populateDropdown = function(countries) {
+  var dropDown = document.querySelector('#country-dropdown');
+  countries.forEach(function(country) {
+    var option = document.createElement('option');
+    option.innerText = country.name;
+    option.setAttribute("value", country.name);
+    dropDown.appendChild(option);
+  })
 }
 
 var app = function(){
-  var button = document.querySelector('#reveal-button');
-  button.addEventListener('click', clickToReveal);
+
+
+  var url = 'https://restcountries.eu/rest/v2';
+  makeRequest(url, requestComplete);
 }
 
 window.addEventListener('load', app);
