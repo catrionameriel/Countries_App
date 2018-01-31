0479cd604a3f4b3d9a9c5cd4bb6ab0fa1b613f9e
catrionameriel <catriona.meriel@gmail.com>
2018-01-31 13:28:05 +0000

    populated dropdown

--- public/index.js
+++ public/index.js
@@ -10,26 +10,38 @@ var clickToReveal = function(){
   makeRequest(url, requestComplete);
 }
 
-var populateList = function(countries){
-  var ul = document.querySelector('#country-list');
-
-  countries.forEach(function(country){
-    var li = document.createElement("li");
-    li.innerText = country.name;
-    ul.appendChild(li);
-  })
-}
+// var populateList = function(countries){
+//   var ul = document.querySelector('#country-list');
+//
+//   countries.forEach(function(country){
+//     var li = document.createElement("li");
+//     li.innerText = country.name;
+//     ul.appendChild(li);
+//   })
+// }
 
 var requestComplete = function(){
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
-  populateList(countries);
+  populateDropdown(countries);
+  // populateList(countries);
+}
+
+var populateDropdown = function(countries) {
+  var dropDown = document.querySelector('#country-dropdown');
+  countries.forEach(function(country) {
+    var option = document.createElement('option');
+    option.innerText = country.name;
+    dropDown.appendChild(option);
+  })
 }
 
 var app = function(){
-  var button = document.querySelector('#reveal-button');
-  button.addEventListener('click', clickToReveal);
+  // var button = document.querySelector('#reveal-button');
+  // button.addEventListener('click', clickToReveal);
+  var url = 'https://restcountries.eu/rest/v2';
+  makeRequest(url, requestComplete);
 }
 
 window.addEventListener('load', app);
